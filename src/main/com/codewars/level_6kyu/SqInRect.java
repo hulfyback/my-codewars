51488063ee8cdafd1ca264c93db4b5e1dfcfa5f6
Zsolt Molnár <hulfyback@gmail.com>
2019-06-29 10:57:20 +0200

    feat(my-codewars): rectangle into squares

--- src/main/com/codewars/level_6kyu/SqInRect.java
+++ src/main/com/codewars/level_6kyu/SqInRect.java
@@ -4,15 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SqInRect {
+  private static List<Integer> listSquares = new ArrayList<>();
+  private static int tempGreaterSide;
+  private static int tempSmallerSide;
+  private static int tempDiff;
 
   public static List<Integer> sqInRect(int lng, int wdth) {
     if (lng == wdth) {
       return null;
     }
-    List<Integer> listSquares = new ArrayList<>();
-    listSquares.add(Math.max(lng, wdth));
-    int temp_diff = Math.min(lng, wdth);
+    tempGreaterSide = Math.min(lng, wdth);
+    tempDiff = Math.abs(lng - wdth);
+    listSquares.add(tempGreaterSide);
 
+    while (tempDiff != tempSmallerSide) {
+      tempSmallerSide = tempDiff;
+      tempDiff = tempGreaterSide - tempSmallerSide;
+      tempGreaterSide = tempSmallerSide;
+      listSquares.add(tempGreaterSide);
+    }
     return listSquares;
   }
 }
